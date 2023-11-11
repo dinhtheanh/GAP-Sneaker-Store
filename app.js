7f8dfa5648a47d3bca55ad2abc7119a7c882a52d
The Anh <dinhtheanh753@gmail.com>
2023-11-11 13:07:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 require('dotenv/config');
-
-
 const homeRoute = require('./src/routes/home.js');
 const pagesRoute = require('./src/routes/pages.js');
 // Server Initialization 
@@ -10,17 +8,10 @@ const app = express();
 // Configure Handlebars
 app.set('views', path.join(__dirname, '/src/views'));
 app.set("view engine", "hbs");
-
 app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT;
-
 app.use("/", homeRoute);
 app.use("/products", pagesRoute);
-
 let server = app.listen(PORT, () => {
-
     console.log(`Server running on port ${PORT}`);
 })
-
-
-
