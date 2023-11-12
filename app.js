2fdae6ccdafd1dd4f95b0224b33b3391a3461b0e
The Anh <dinhtheanh753@gmail.com>
2023-11-12 18:55:21 +0700

    Add comments in app.js

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+// Require necessary packages
 const express = require('express');
 const path = require('path');
 require('dotenv/config');
 const navigationRoute = require('./src/routes/navigation.js');
+
 // Server Initialization 
 const app = express();
-// Configure Handlebars
+
+// Configure Handlebars View Engine
 app.set('views', path.join(__dirname, '/src/views'));
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, 'public')));
-const PORT = process.env.PORT;
+app.use(express.static(path.join(__dirname, 'public')))
 app.use("/", navigationRoute);
 
-
+// Start the server
+const PORT = process.env.PORT;
 let server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
