bc020e411e399b7fef80711ce4eb03889731de7f
The Anh <dinhtheanh753@gmail.com>
2023-11-18 10:31:03 +0700

    Format app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // Require necessary packages
-
 const express = require('express');
 const path = require('path');
 require('dotenv/config');
 let initRouteWeb = require('./src/routes/navigation.js');
+
 // Server Initialization 
 const app = express();
 initRouteWeb(app);
+
 // Configure Handlebars View Engine
 app.use(express.static(path.join(__dirname, 'public/')));
-
 app.set('views', path.join(__dirname, '/src/views'));
 app.set("view engine", "hbs");
 
@@ -18,8 +18,6 @@ const PORT = process.env.PORT;
 let server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
-
-// Replace the uri string with your connection string.
 
   
 module.exports = app
